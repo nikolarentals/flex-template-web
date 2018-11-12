dad225d27b2d0092760e933f6464a684d5360478
Jenni Nurmi <jenni@sharetribe.com>
2018-11-12 10:34:13 +0200

    Add FilterForm component

--- src/examples.js
+++ src/examples.js
@@ -71,6 +71,7 @@ import * as EditListingPoliciesForm from './forms/EditListingPoliciesForm/EditLi
 import * as EditListingPricingForm from './forms/EditListingPricingForm/EditListingPricingForm.example';
 import * as EmailVerificationForm from './forms/EmailVerificationForm/EmailVerificationForm.example';
 import * as EnquiryForm from './forms/EnquiryForm/EnquiryForm.example';
+import * as FilterForm from './forms/FilterForm/FilterForm.example';
 import * as LoginForm from './forms/LoginForm/LoginForm.example';
 import * as PasswordRecoveryForm from './forms/PasswordRecoveryForm/PasswordRecoveryForm.example';
 import * as PasswordResetForm from './forms/PasswordResetForm/PasswordResetForm.example';
@@ -115,6 +116,7 @@ export {
   FieldReviewRating,
   FieldSelect,
   FieldTextInput,
+  FilterForm,
   Footer,
   IconBannedUser,
   IconCheckmark,
